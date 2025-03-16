066b612ef0d14a11d896ea017db3b565c6a450ea
Ethan Arnold <145302595+Ethan-cmd123@users.noreply.github.com>
2025-03-16 20:34:18 +1300

    Added apt-get install script

---
+++ install-pygame-deps.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+sudo apt-get install linux-libc-dev
+#sudo apt-get install python3-sdl2
+sudo apt-get install cmake
+sudo apt-get install libsdl2-2.0-0
+sudo apt-get install libsdl2-dev
+sudo apt-get install libsdl2-image-dev
+sudo apt-get install libsdl2-mixer-dev
+sudo apt-get install libsdl2-ttf-dev
+sudo apt-get install libsdl2-gfx-dev
+sudo apt-get install libportmidi-dev
+sudo apt-get install espeak-ng
+sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
+sudo apt-get install python-dev-is-python3 
+sudo apt-get install flac 
